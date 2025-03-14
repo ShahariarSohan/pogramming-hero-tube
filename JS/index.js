739f25a0df816a9aed455e695ce8e8a27cd997f4
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-03-14 15:40:22 +0600

    all content showed in main page

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+// display button
 const displayButton = async () => {
     const res = await fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     const data = await res.json()
@@ -13,3 +14,29 @@ const displayButton = async () => {
     });
 }
 displayButton();
+
+const displayVideos = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    const data = await res.json()
+    const videoData = data.videos;
+    videoData.forEach(element => {
+        const contentContainer = document.getElementById("content-container")
+        const card = document.createElement("div")
+        card.innerHTML = `
+        <div class="card bg-base-100 shadow-sm">
+           <figure>
+             <img class=" h-48 w-96 object-cover"
+               src="${element.thumbnail}"
+               alt="Videos" />
+           </figure>
+           <div class="card-body">
+             <h2 class="card-title">Card Title</h2>
+             <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+           </div>
+         </div>
+        `
+        contentContainer.append(card)
+
+    });
+}
+displayVideos()
